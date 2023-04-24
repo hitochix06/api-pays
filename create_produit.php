bcdc471c15b1257220a6cf6045b5267e41a678e2
hitochix06 <pachara.philibert@hotmail.fr>
2023-04-24 18:25:59 +0200

    rajouter notat

--- create_produit.php
+++ create_produit.php
@@ -6,6 +6,8 @@ $pdo = pdo_connect_mysql();
 if (isset($_POST['bouton'])) {
   $id = isset($_POST['id']) && !empty($_POST['id']) && $_POST['id'] != 'auto' ? $_POST['id'] : NULL;
   $nom = isset($_POST['nom']) ? $_POST['nom'] : '';
+  $ville = isset($_POST['ville']) ? $_POST['ville'] : '';
+  $note = isset($_POST['note']) ? $_POST['note'] : '';
   // image 
   $image = $_FILES["image"]["name"];
   $tmpname = $_FILES["image"]["tmp_name"];
@@ -16,8 +18,8 @@ if (isset($_POST['bouton'])) {
   $description = isset($_POST['description']) ? $_POST['description'] : '';
   $categorie = isset($_POST['categorie']) ? $_POST['categorie'] : '';
   $date_sortie = isset($_POST['date_sortie']) ? $_POST['date_sortie'] : '';
-  $query = $pdo->prepare('INSERT INTO `produits`(`id`,`nom`, `image`, `description`, `date_sortie`, `categorie_id`) VALUES (?,?,?,?,?,?)');
-  $query->execute([$id, $nom, $image, $description, $date_sortie, $categorie]);
+  $query = $pdo->prepare('INSERT INTO `produits`(`id`,`nom`, `image`, `description`, `date_sortie`, `note`, `ville`, `categorie_id`) VALUES (?,?,?,?,?,?,?,?)');
+  $query->execute([$id, $nom, $image, $description, $note, $date_sortie, $ville, $categorie]);
 }
 
 
